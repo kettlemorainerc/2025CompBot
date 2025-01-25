28bdf11fd616582521f456408b85d080c0212c3f
KettlemoraineRC <kettlemorainerc@gmail.com>
2025-01-25 14:47:06 -0600

    Align to coral with LimeLight

--- src/main/java/org/usfirst/frc/team2077/common/command/NewTest.java
+++ src/main/java/org/usfirst/frc/team2077/common/command/NewTest.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team2077.common.command;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.usfirst.frc.team2077.RobotHardware;
 import org.usfirst.frc.team2077.common.subsystem.MotorRun;
 
@@ -16,17 +19,20 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.util.RuntimeLoader;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.LimelightHelpers;
 import pabeles.concurrency.IntOperatorTask.Max;
 
 public class NewTest extends RepeatedCommand {
 
     public enum Direction{
         FORWARD,
-        BACKWARD
+        BACKWARD,
+        AUTO
     }
 
-    // private final Launcher launcher;
-    // private final Launcher.Target target;
+    private static final int[] CORAL_APRIL_TAGS = new int[]{6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22};
 
     private final MotorRun motorRun;
     private final Direction direction;
@@ -44,9 +50,27 @@ public class NewTest extends RepeatedCommand {
         // launcher.run(target);
         if (direction == Direction.FORWARD) {
             motorRun.startForward();
-        } else {
+        } else if (direction == Direction.BACKWARD) {
             motorRun.startBackward();
+        } else {
+            double tx = LimelightHelpers.getTX("");
+            int tid = (int) LimelightHelpers.getFiducialID("");
+            if(Arrays.stream(CORAL_APRIL_TAGS).anyMatch(i -> i == tid)){
+                if (tx > 2) {
+                    motorRun.startForward();
+                    // boolean hasTarget = LimelightHelpers.getTV("");
+                    // SmartDashboard.putBoolean("Target", hasTarget);
+                }else if(tx < -2){
+                    motorRun.startBackward();
+                }else{
+                    motorRun.stopMotor();
+                }
+            }else{
+                motorRun.startForward();
+            }
+
         }
+
         
 
     }
