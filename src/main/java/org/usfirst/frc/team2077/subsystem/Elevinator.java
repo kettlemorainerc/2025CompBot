30544703794ce084b94923ccf8b12e1ea8e4334f
KettlemoraineRC <kettlemorainerc@gmail.com>
2025-02-04 19:11:41 -0600

    added elevinator functions

--- src/main/java/org/usfirst/frc/team2077/subsystem/Elevinator.java
+++ src/main/java/org/usfirst/frc/team2077/subsystem/Elevinator.java
@@ -1,14 +1,47 @@
 package org.usfirst.frc.team2077.subsystem;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Elevinator implements Subsystem{
     private final TalonSRX leftLifter;
     private final TalonSRX rightLifter;
+    private final DigitalInput bottomRightSwitch;
+    private final DigitalInput bottomLeftSwitch;
 
     public Elevinator() {
-        leftLifter = new TalonSRX(21);
+        leftLifter = new TalonSRX(16);
         rightLifter = new TalonSRX(22);
+        bottomRightSwitch = new DigitalInput(0);
+        bottomLeftSwitch = new DigitalInput(0);
+    }
+
+    public void raiseElevinators(){
+        leftLifter.set(ControlMode.PercentOutput, 0.5);
+        rightLifter.set(ControlMode.PercentOutput, 0.5);
+    }
+
+    public void lowerElevinators(){
+        leftLifter.set(ControlMode.PercentOutput, -0.5);
+        rightLifter.set(ControlMode.PercentOutput, -0.5);
+    }
+
+    public void raiseRightElevinator(){
+        rightLifter.set(ControlMode.PercentOutput, 0.5);
+    }
+
+    public void raiseLeftElevinator(){
+        leftLifter.set(ControlMode.PercentOutput, 0.5);
+    }
+
+    public void lowerRightElevinator(){
+        rightLifter.set(ControlMode.PercentOutput, -0.5);
+    }
+
+    public void lowerLeftElevinator(){
+        leftLifter.set(ControlMode.PercentOutput, -0.5);
     }
 }
