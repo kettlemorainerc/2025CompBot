3e3bb8c11234a43afe0b3731439f6be6fe195d95
Dustin_Cowell <homercow3@gmail.com>
2025-02-18 19:02:07 -0600

    Basics remade for new elevator system, will now work on encoder and positions

--- src/main/java/org/usfirst/frc/team2077/subsystem/Elevator.java
+++ src/main/java/org/usfirst/frc/team2077/subsystem/Elevator.java
@@ -3,31 +3,44 @@ package org.usfirst.frc.team2077.subsystem;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class Elevator implements Subsystem {
 
-    private static final Cans
-            elevatorMotor = new CanSP(1
+    private final SparkMax elevatorMotor;
+    private final SparkClosedLoopController maxPID;
+
 
 
     public Elevator(){
-        ;
+        elevatorMotor = new SparkMax(1, SparkLowLevel.MotorType.kBrushless);
+        maxPID = elevatorMotor.getClosedLoopController();
+        SparkMaxConfig config = new SparkMaxConfig();
+        config
+                .inverted(false)
+                .idleMode(SparkBaseConfig.IdleMode.kBrake);
+        config.encoder
+                .positionConversionFactor(1000)
+                .velocityConversionFactor(1000);
+        config.closedLoop
+                .feedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder)
+                .pid(1.0,0.0,0.0);
+        elevatorMotor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
     }
     /*raises the elevator w(ﾟДﾟ)w,
     all numbers are placeholders in all files made in this branch unless stated otherwise stated
     this will be placed in other places as well to remind you to remind you of this
     */
 
-    public static void raise(){
-        elevatorMotor.set(TalonSRXControlMode.PercentOutput, .5);
-    }
-    public static void lower(){
-        elevatorMotor.set(TalonSRXControlMode.PercentOutput, -.5);
-    }
+    public void raise(){elevatorMotor.set(0.01);}
+    public void lower(){elevatorMotor.set(-0.01);}
 
-    public static void stop(){
-        elevatorMotor.set(ControlMode.PercentOutput, 0);
-    }
-
+    public void stop(){elevatorMotor.set(0);}
 }
