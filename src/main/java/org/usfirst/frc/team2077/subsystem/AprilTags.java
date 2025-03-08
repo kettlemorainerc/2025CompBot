4f2a89cbdd53a73faaa5567f970f9b8f618b2257
kettlemorainerc <kettlemorainerc@gmail.com>
2025-03-07 19:13:53 -0600

    Pulled in April tag constants

---
+++ src/main/java/org/usfirst/frc/team2077/subsystem/AprilTags.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team2077.subsystem;
+
+public class AprilTags {
+    public final static int[] CORAL_STATION = {6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22};
+    public final static int[] PICKUP_STATION = {1, 2, 12, 13};
+    public final static int[] PROCESSING_STATION = {3, 16};
+}
