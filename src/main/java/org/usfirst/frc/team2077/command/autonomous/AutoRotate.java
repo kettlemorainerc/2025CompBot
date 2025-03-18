7cf55928c2d7a8740a2b79f8459d268daea70f16
Dustin_Cowell <homercow3@gmail.com>
2025-03-18 17:48:22 -0500

    fixed build issue that came from no where lol

---
+++ src/main/java/org/usfirst/frc/team2077/command/autonomous/AutoRotate.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team2077.command.autonomous;
+
+import com.studica.frc.AHRS;
+import edu.wpi.first.wpilibj2.command.Command;
+import org.usfirst.frc.team2077.common.drivetrain.AbstractChassis;
+
+public class AutoRotate extends Command {
+     private AbstractChassis chassis;
+
+     private double targetAngle;
+     private double gyroAngle;
+     private double initialAngle;
+     private AHRS gyro = new AHRS(AHRS.NavXComType.kMXP_SPI);
+
+     public AutoRotate(double angle){
+          targetAngle = angle;
+     }
+     @Override
+     public void initialize(){
+          initialAngle = gyro.getAngle();
+     }
+     @Override
+     public void execute(){
+          gyroAngle = initialAngle - gyroAngle;
+
+          if (isFinished()){
+               this.end(false);
+               chassis.setVelocity(0, 0, 0.3*Math.signum(targetAngle-initialAngle));
+          }
+     }
+     @Override
+     public void end(boolean interrupted){
+          chassis.halt();
+     }
+     @Override
+     public boolean isFinished(){
+          return (((targetAngle + 0.05) >= gyroAngle) || (gyroAngle >= targetAngle - 0.05));
+
+     }
+}
