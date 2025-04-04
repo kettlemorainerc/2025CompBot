7bad449a4c6609a2b1d9a7b3ebbf5d85bda51c14
Dustin_Cowell <homercow3@gmail.com>
2025-04-04 10:44:28 -0500

    bais manager is done, trying to get a coral pickup script done

--- src/main/java/org/usfirst/frc/team2077/Robot.java
+++ src/main/java/org/usfirst/frc/team2077/Robot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2077;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj2.command.*;
+import org.usfirst.frc.team2077.command.autonomous.AutonomousManager;
 
 public class Robot extends TimedRobot {
     private RobotHardware hardware;
@@ -26,7 +27,7 @@ public class Robot extends TimedRobot {
      * When you click the "Autonomous" option in driver station
      */
     @Override public void autonomousInit() {
-
+        new AutonomousManager(AutonomousManager.AUTONOMOUSTYPE.BASIC_MOVE);
     }
 
     /**
